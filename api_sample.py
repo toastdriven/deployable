535304b693c61ccbb3f4bd302b4817f950479510
Daniel Lindsley <daniel@toastdriven.com>
2009-09-06 22:28:11 -0500

    Fixed case on sample to reflect classes, not functions.

--- api_sample.py
+++ api_sample.py
@@ -6,10 +6,10 @@ TARGET_DIRECTORY = os.path.join('/Users/daniellindsley/Desktop/sample_deploy')
 
 
 install_list = [
-    tarball(url='http://pypi.python.org/packages/source/W/Whoosh/Whoosh-0.3.0b24.zip', postprocess=whoosh_post),
-    # git(url='git://github.com/toastdriven/django-haystack.git', revision='b44afc6c'),
-    # svn(url='http://code.djangoproject.com/svn/django/trunk/django'),
-    # git_svn(url='http://code.djangoproject.com/svn/django/trunk/django'),
+    Tarball(url='http://pypi.python.org/packages/source/W/Whoosh/Whoosh-0.3.0b24.zip', post_process=whoosh_post),
+    # Git(url='git://github.com/toastdriven/django-haystack.git', revision='b44afc6c'),
+    # Svn(url='http://code.djangoproject.com/svn/django/trunk/django'),
+    # GitSvn(url='http://code.djangoproject.com/svn/django/trunk/django'),
 ]
 
 
